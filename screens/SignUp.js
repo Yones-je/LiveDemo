f4792c640d37bfbf83eb69b1845cf72f5ca8f389
Yones-je <59484418+Yones-je@users.noreply.github.com>
2021-01-12 19:44:17 +0100

    File Structure

---
+++ screens/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const signUp = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default signUp;
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import Home from './Home';
+import Live from './Live';
+import SignUp from './SignUp';
+
+export {Home, Live, SignUp};
