118ffb9f59c1f7eafa53761cac5106691f63cc93
Yones-je <59484418+Yones-je@users.noreply.github.com>
2021-01-15 17:53:40 +0100

    Assets & Theme Setup

---
+++ constants/icons.js
@@ -0,0 +1,19 @@
+const bookshelf = require('../assets/icons/bookshelf.png');
+const camera = require('../assets/icons/camera.png');
+const carpet = require('../assets/icons/carpet.png');
+const desktop = require('../assets/icons/desktop.png');
+const dress = require('../assets/icons/dress.png');
+const live = require('../assets/icons/live.png');
+const shirt = require('../assets/icons/shirt.png');
+const streaming = require('../assets/icons/streaming.png');
+
+export default {
+  bookshelf,
+  camera,
+  carpet,
+  desktop,
+  dress,
+  live,
+  shirt,
+  streaming,
+};
---
+++ constants/images.js
@@ -0,0 +1,7 @@
+const liveLogo = require('../assets/images/liveLogo.png');
+const streamingLogo = require('../assets/images/streamingLogo.png');
+
+export default {
+  liveLogo,
+  streamingLogo,
+};
